7fbdf4d1182f9182daaba90266c436761c2a4eff
mogeiwang <mogeiwang@gmail.com>
2016-06-11 23:40:40 +0800

    first commit

---
+++ imports.scala
@@ -0,0 +1,13 @@
+#! /usr/bin/env scala
+
+// imports
+import scala.actors._  // Actor
+import scala.collection.JavaConversions._  // Java ops
+import scala.collection.mutable._  // ArrayBuffer, Buffer, HashMap, Map, ...
+import scala.io.Source._  // file ops
+import scala.math._  // the math
+import scala.sys.process._  // work with shell
+import scala.util.control.Breaks._  // break loops
+import scala.util.parsing._  // parsing toolbox
+import scala.util.Sorting._  // quickSort etc
+import scala.util.continuations._  // cont...
