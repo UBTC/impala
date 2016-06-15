32b6530b2267580b6ab42789071fcf3f3845feb6
mogeiwang <mogeiwang@gmail.com>
2016-06-15 10:04:51 +0800

    Add immutable, regex, reflect importing

--- Predef.scala
+++ Predef.scala
@@ -1,12 +1,15 @@
 // imports
 import scala.actors._  // Actor
 import scala.actors.Actor._  // actions
+import scala.collection.immutable._  // Vector, List, Range, HashMap or HashSet
+import scala.collection.mutable._  // ArrayBuffer, StringBuilder, HashMap or HashSet
 import scala.collection.JavaConversions._  // Java ops
-import scala.collection.mutable._  // ArrayBuffer, Buffer, HashMap, Map, ...
 import scala.io.Source._  // file ops
 import scala.math._  // the math
+import scala.reflect.runtime.universe._  // reflection
 import scala.sys.process._  // work with shell
+import scala.util.continuations._  // cont...
 import scala.util.control.Breaks._  // break loops
+import scala.util.matching.Regex._  // regex
 import scala.util.parsing._  // parsing toolbox
 import scala.util.Sorting._  // quickSort etc
-import scala.util.continuations._  // cont...
