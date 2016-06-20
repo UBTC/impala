f9c17b2a1fc265ec71651aad3c30ff4ea76d7efd
M.W <mogeiwang@gmail.com>
2016-06-20 12:09:27 +0800

    Use akka actor

--- Predef.scala
+++ Predef.scala
@@ -1,13 +1,15 @@
 // imports
-import scala.actors._  // Actor
-import scala.actors.Actor._  // actions
+// import scala.actors._  // Actor
+// import scala.actors.Actor._  // actions
 import scala.collection.immutable.{Map => _, Set => _, _}  // Import Vector, List, Range; excludes Map, Set
 import scala.collection.mutable._  // ArrayBuffer, StringBuilder, HashMap or HashSet
 import scala.collection.JavaConversions._  // Java ops
+import scala.concurrent.duration._ // actor msg duration
 import scala.io.Source._  // file ops
 import scala.language._  // postfixOps
 import scala.math._  // the math
 import scala.reflect.runtime.universe._  // reflection
+import scala.runtime._ // runtime things
 import scala.sys.process._  // work with shell
 import scala.util.continuations._  // cont...
 import scala.util.control.Breaks._  // break loops
@@ -15,3 +17,8 @@ import scala.util.matching.Regex._  // regex
 import scala.util.parsing._  // parsing toolbox
 import scala.util.Random._  // random numbers
 import scala.util.Sorting._  // quickSort etc
+import akka.actor._ // use akka actor
+import akka.event._
+import akka.dispatch._
+import akka.pattern._
+import akka.util._
